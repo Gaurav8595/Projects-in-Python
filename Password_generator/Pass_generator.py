8b365c082ec0e598c4aad123a9402b673abc8378
Gaurav8595 <98168267+Gaurav8595@users.noreply.github.com>
2022-01-21 20:06:00 +0530

    Add files via upload

--- Password_generator/Pass_generator.py
+++ Password_generator/Pass_generator.py
@@ -1,1 +1,13 @@
-
+#Random password Generator:
+import random
+alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+while 1:
+    pass_count = int(input("Enter the no. of password you want : "))
+    pass_digit = int(input("Enter the no. of digit you want in yor password: "))
+    for i in range(0, pass_count):
+        password = ""
+        for a in range(0, pass_digit):
+            password_char = random.choice(alpha)
+            password = password + password_char
+        print("Yor password is : ", password)
+    break
